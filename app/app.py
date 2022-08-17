56e943d42ea62a403081b919ef8f06eac53211ea
Toby <tdudley@live.co.uk>
2022-08-17 21:40:11 +0100

    lambda function for exif stripping

--- app/app.py
+++ app/app.py
@@ -1,24 +1,53 @@
 import boto3
 import os
 from PIL import Image
+import logging
 
-batch = boto3.client('batch')
-s3 = boto3.client('s3')
-ssm = boto3.client('ssm')
+s3 = boto3.resource('s3')
+s3_client = boto3.client('s3')
 
-S3_BUCKET = os.environ.get('S3_BUCKET_A', 'gel_bucket_a')
-S3_BUCKET = os.environ.get('S3_BUCKET_B', 'gel_bucket_b')
+S3_BUCKET_A = os.environ.get('S3_BUCKET_A', 'gel-bucket-a')
+S3_BUCKET_B = os.environ.get('S3_BUCKET_B', 'gel-bucket-b')
 
 def handler(event, context):
 
-    print(event)
+    key = event['Records'][0]['s3']['object']['key']
+    print(key)
+    logging.info(f"Detected file uploaded to {S3_BUCKET_A} with name {key}")
 
-    # image = Image.open('test.jpg')
+    download_file_from_s3(key, S3_BUCKET_A)
+    logging.info(f"Downloaded {key}")
 
-    # # next 3 lines strip exif
-    # data = list(image.getdata())
-    # image_without_exif = Image.new(image.mode, image.size)
-    # image_without_exif.putdata(data)
+    strip_exif(key)
+    logging.info(f"Stripped exif data from {key}")
 
-    # image_without_exif.save('image_file_without_exif.jpeg')
+    upload_file_to_s3('exif_stripped.jpg', S3_BUCKET_B, key)
+    logging.info(f"Uploaded {key} to {S3_BUCKET_B}")
 
+    return 200
+
+def download_file_from_s3(key: str, bucket: str):
+    try:
+        s3.Bucket(S3_BUCKET_A).download_file(key, key)
+    except NameError as e:
+        if e.response['Error']['Code'] == "404":
+            logging.error(f"The object {key} does not exist.")
+        else:
+            raise
+
+def upload_file_to_s3(file_name: str, bucket: str, key: str):
+    try:
+        response = s3_client.upload_file(file_name, S3_BUCKET_B, key)
+    except NameError as e:
+        raise logging.error(e)
+
+def strip_exif(image: str):
+    try:
+        image = Image.open(image)
+        data = list(image.getdata())
+        image_without_exif = Image.new(image.mode, image.size)
+        image_without_exif.putdata(data)
+
+        image_without_exif.save('exif_stripped.jpg')
+    except Exception as e:
+        raise logging.error(e)
